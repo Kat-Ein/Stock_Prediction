9a89247e051f7355d70d45ffa86528bb2e356b32
Kat-Ein <katein0318@gmail.com>
2026-04-07 18:12:41 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -99,8 +99,8 @@ def call_model_api(input_df):
     predictor = Predictor(
         endpoint_name=MODEL_INFO["endpoint"],
         sagemaker_session=sm_session,
-        serializer=NumpySerializer(),
-        deserializer=JSONDeserializer() 
+        serializer=JSONSerializer(),
+        deserializer=NumpyDeserializer() 
     )
 
     try:
