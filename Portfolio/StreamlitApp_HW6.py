16d8e04ef4170059884dfbd7544b2b1b73f42aae
Kat-Ein <katein0318@gmail.com>
2026-04-23 16:34:25 -0500

    Refactor imports and update explainer filename
    Updated the import statements and adjusted the model explainer filename.

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -61,7 +61,7 @@ df_features = extract_features()
 
 MODEL_INFO = {
         "endpoint": aws_endpoint,
-        "explainer": 'explainer_sentiment.shap',
+        "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_sentiment_model.tar.gz',
         "keys": ['ADBE', 'AMZN', 'MSFT', 'sentiment_textblob', 'EMA_5', 'MA_5', 'RSI_5',
        'ROC_5', 'MOM_5'],
